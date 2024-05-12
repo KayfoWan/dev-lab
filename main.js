3f125da817d9f196358509d455617a57791c4742
k4199 <kai.worley4@gmail.com>
2024-05-11 23:26:56 -0500

    line demo

--- main.js
+++ main.js
@@ -1,52 +1,62 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import WebGL from "three/addons/capabilities/WebGL.js";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+if( WebGL.isWebGLAvailable()) {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-//const controls = new OrbitControls( camera, renderer.domElement );
-//const loader = new GLTFLoader();
+    const canvas = document.getElementById("canvas");
+    const canvasHeight = canvas.clientHeight;
+    const canvasWidth = canvas.clientWidth;
+    const renderer = new THREE.WebGLRenderer({canvas: canvas});
+    renderer.setSize(canvasWidth, canvasHeight);
+    //renderer.setSize( window.innerWidth, window.innerHeight );
+    //document.body.appendChild( renderer.domElement );
 
-//renderer.setClearColor("#Ff6500");
+    //const controls = new OrbitControls( camera, renderer.domElement );
+    //const loader = new GLTFLoader();
 
-// setClearColor sets the background
-// takes in HEX values
+    //renderer.setClearColor("#Ff6500");
 
-// Types of cameras
-// 1. Perspective (THREE.PerspectiveCamera(FOV, Aspect Ratio, Near Clipping Plane, Far Clipping Plane))
-// objects farther than the far Clipping Plane and closer to the Near Clipping Plane won't be rendered
+    // setClearColor sets the background
+    // takes in HEX values
 
-// To render at a lower resolution add a third boolean attribute to the setSize method and set it to false
+    // Types of cameras
+    // 1. Perspective (THREE.PerspectiveCamera(FOV, Aspect Ratio, Near Clipping Plane, Far Clipping Plane))
+    // objects farther than the far Clipping Plane and closer to the Near Clipping Plane won't be rendered
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
-const cube = new THREE.Mesh( geometry, material);
-scene.add(cube);
+    // To render at a lower resolution add a third boolean attribute to the setSize method and set it to false
 
-camera.position.z = 5;
+    const geometry = new THREE.BoxGeometry( 1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
+    const cube = new THREE.Mesh( geometry, material);
+    scene.add(cube);
 
-// Types of materials
-// 1. MeshBasicMaterial(Hex Color, )
-// everything added to the scene will be added to point 0,0,0
+    camera.position.z = 5;
 
-function animate() {
-    requestAnimationFrame(animate);
+    // Types of materials
+    // 1. MeshBasicMaterial(Hex Color, )
+    // everything added to the scene will be added to point 0,0,0
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    function animate() {
+        requestAnimationFrame(animate);
 
-    renderer.render(scene, camera);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    };
+    animate();
+
+    // requestAnimationFrame() has many advantages over setInterval() but a big one is that pauses when the user 
+    // navigates to another browser tab, hence not wasting their precious processing power and battery life.
+
+} else {
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById( 'errorContainer' ).appendChild( warning );
 };
-animate();
-
-// requestAnimationFrame() has many advantages over setInterval() but a big one is that pauses when the user 
-// navigates to another browser tab, hence not wasting their precious processing power and battery life.
-
-
 
 
