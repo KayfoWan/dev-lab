fed4a31cced1b282405e4498925a53c5cde4d6c9
k4199 <kai.worley4@gmail.com>
2024-08-02 01:30:22 -0500

    Tile Map

--- src/scripts/tileMap/game.js
+++ src/scripts/tileMap/game.js
@@ -3,7 +3,7 @@ import { Camera } from './camera.js';
 
 const $ = elem=>document.getElementById(elem);
 const GAME_WIDTH = 512; // max 768 x 768
-const GAME_HEIGHT = 512;
+const GAME_HEIGHT = 640;
 
 class Game {
     constructor() {
@@ -34,18 +34,45 @@ class Game {
         else if (this.keys[0] === 's') speedY = 1;
         this.camera.move(deltaTime, speedX, speedY);
     }
+    drawLayer(layer, ctx) {
+        const startCol = Math.floor(this.camera.x / this.map.tileSize);
+        const endCol = startCol + (this.camera.width / this.map.tileSize);
+        const startRow = Math.floor(this.camera.y / this.map.tileSize);
+        const endRow = startRow + (this.camera.height / this.map.tileSize);
+        const offsetX = -this.camera.x + startCol * this.map.tileSize;
+        const offsetY = -this.camera.y + startRow * this.map.tileSize;
+
+        for (let row = startRow; row <= endRow; row++) {
+            for (let col = startCol; col <= endCol; col++) {
+                const tile = this.map.getTile(layer, col, row);
+                const x = (col - startCol) * this.map.tileSize + offsetX;
+                const y = (row - startRow) * this.map.tileSize + offsetY;
+                ctx.drawImage(
+                    this.map.image,
+                    (tile - 1) * this.map.image_tile % this.map.image.width,
+                    Math.floor((tile - 1) / this.map.image_cols) * this.map.image_tile,
+                    this.map.image_tile,
+                    this.map.image_tile,
+                    Math.round(x),
+                    Math.round(y),
+                    this.map.tileSize,
+                    this.map.tileSize
+                );
+                /*
+                ctx.strokeStyle = 'white';
+                ctx.strokeRect(
+                    col * this.map.tileSize,
+                    row * this.map.tileSize,
+                    this.map.tileSize,
+                    this.map.tileSize
+                );
+                */
+            }
+        };
+    }
     render(ctx) {
-        ctx.drawImage(
-            this.map.image,
-            this.camera.x,
-            this.camera.y,
-            GAME_WIDTH,
-            GAME_HEIGHT,
-            0,
-            0,
-            GAME_WIDTH,
-            GAME_HEIGHT
-        );
+        this.drawLayer(0, ctx);
+        this.drawLayer(1, ctx);
     }
 }
 
@@ -54,6 +81,7 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext('2d');
     canvas.width = GAME_WIDTH;
     canvas.height = GAME_HEIGHT;
+    ctx.imageSmoothingEnabled = false;
 
     //ctx.fillStyle = "white";
     //ctx.fillRect(10,10,150,20);
