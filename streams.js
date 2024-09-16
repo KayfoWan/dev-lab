9a41dd21cd1520d56270d63363060d4f2d11fb4b
k4199 <kai.worley4@gmail.com>
2024-09-16 02:54:46 -0500

    streams

---
+++ streams.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer();
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log('Server has started!');
+});
+
+server.on('request', (req, res)=>{
+    // Imagine the file is actually larger than it is.
+    /*
+    fs.readFile('./files/largeFile.txt', (err, data)=>{
+        if(err) {
+            res.end('Something went wrong!');
+            return;
+        }
+        res.end(data);
+    })
+    */
+    server.on('request', (req, res)=>{
+        let rs = fs.createReadStream('./files/largeFi2le.txt');
+        rs.on('data', (chunk)=>{
+            res.write(chunk);
+            res.end();
+        });
+        rs.on('error', (error)=>{
+            res.end(error.message);
+        });
+    })
+})
