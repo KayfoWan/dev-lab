54d43362d9574d3ea63e2b262a7ed0bc68229663
k4199 <kai.worley4@gmail.com>
2024-10-07 17:54:48 -0500

    transfer

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,6 @@ module.exports = {
     entry: './main.js', // Specify the entry point
     output: {
         filename: 'bundle.js', // Name of the output bundle
-        path: __dirname + '/dist' // Output directory (create 'dist' folder if it doesn't exist)
+        path: __dirname + '/dist2' // Output directory (create 'dist' folder if it doesn't exist)
     }
 };
